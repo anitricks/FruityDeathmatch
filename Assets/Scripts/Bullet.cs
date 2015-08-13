14ab21780be7266822eb95c5c3ed14d1465248e0
Trishant Bhatija <anitricks@live.com>
2015-08-13 00:04:12 -0400

    First Upload

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+		public GameObject explosion;
+		// Use this for initialization
+		void Start ()
+		{
+			
+				Destroy (gameObject, 2);
+		}
+	
+		// Update is called once per frame
+		void OnTriggerEnter2D (Collider2D other)
+		{
+
+				if (other.tag == "Dummy") {
+						Destroy (gameObject);
+						Debug.Log ("Hit");
+				}
+		}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+
+		public float moveSpeed = 10f;
+		public float jumpForce = 700f;
+		public bool facingR = true;
+
+		Animator anim;
+
+		bool grounded = false;
+		public Transform groundCheck;
+		public LayerMask groundLayer;
+		bool doubleJump = false;
+
+		// Use this for initialization
+		void Start ()
+		{
+				anim = GetComponent<Animator> ();
+		}
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+				grounded = Physics2D.OverlapCircle (groundCheck.position, 0.2f, groundLayer);
+				anim.SetBool ("Grounded", grounded);
+
+				if (grounded)
+						doubleJump = false;
+			
+				float move = Input.GetAxis ("Horizontal");
+
+				anim.SetFloat ("MoveSpeed", Mathf.Abs (move));
+			
+				GetComponent<Rigidbody2D>().velocity = new Vector2 (move * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+				if (move > 0 && !facingR)
+						Flip ();
+				else if (move < 0 && facingR)
+						Flip ();
+
+				anim.SetFloat ("JumpSpeed", GetComponent<Rigidbody2D>().velocity.y);
+				
+				
+		}
+
+		void Update ()
+		{
+			
+				if ((grounded || !doubleJump) && Input.GetKeyDown (KeyCode.Space)) {
+						GetComponent<Rigidbody2D>().AddForce (new Vector2 (0, jumpForce));
+
+						if (!grounded && !doubleJump)
+								doubleJump = true;
+				}
+
+		}
+
+		void Flip ()
+		{
+			
+				facingR = !facingR;
+				Vector3 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;
+		}
+}
